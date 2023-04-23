232c2e8a4bb22ee1005b4049dacdad4d35fb6c0f
Uthaeus <romanlavery@gmail.com>
2023-04-22 20:33:04 -0600

    created api call for quotes, added styling

--- src/quote/quote.js
+++ src/quote/quote.js
@@ -1,35 +1,47 @@
 import { useState, useEffect } from "react";
 
 function Quote() {
-  const [quote, setQuote] = useState("");
-  const [author, setAuthor] = useState("");
+  const [quote, setQuote] = useState("The best way to get started is to quit talking and begin doing.");
+  const [author, setAuthor] = useState("Walt Disney");
   const [quotes, setQuotes] = useState([]);
 
-  useEffect(() => {
-    fetch("https://type.fit/api/quotes")
-      .then((response) => response.json())
-      .then((data) => {
-        setQuotes(data);
-        setQuote(data[0].text);
-        setAuthor(data[0].author);
-      })
-      .catch((error) => {
-        console.log("useEffect quote generator", error);
-      });
-  }, []);
+//   useEffect(() => {
+//     fetch("https://type.fit/api/quotes")
+//       .then((response) => response.json())
+//       .then((data) => {
+//         setQuotes(data);
+//         setQuote(data[0].text);
+//         setAuthor(data[0].author);
+//       })
+//       .catch((error) => {
+//         console.log("useEffect quote generator", error);
+//       });
+//   }, []);
+
+    const getQuoteHandler = () => {
+        const randomNum = Math.floor(Math.random() * quotes.length);
+        setQuote(quotes[randomNum].text);
+        setAuthor(quotes[randomNum].author);
+    };
+
+    const sendTweetHandler = () => {
+        window.open(`https://twitter.com/intent/tweet?text=${quote} - ${author}`);
+    };
 
   return (
     <div className="quote-container">
-      <h1>Quote Generator</h1>
+      <h1 className="w-50 text-left border-bottom">Quote Generator</h1>
 
       <div className="quote-box">
-        <div className="quote-text"></div>
-        <div className="quote-author"></div>
+        <div className="w-100 row">
+            <div className="quote-text col-sm-8">{quote}</div>
+            <div className="quote-author col-sm-3">-{author}</div>
+        </div>
       </div>
 
       <div className="quote-buttons">
-        <button className="quote-button">New Quote</button>
-        <button className="quote-button">Tweet Quote</button>
+        <button className="quote-button" onClick={getQuoteHandler}>New Quote</button>
+        <button className="quote-button tweet" onClick={sendTweetHandler}><i class="bi bi-twitter"></i></button>
       </div>
     </div>
   );
