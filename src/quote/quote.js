ced6723fbf820c89c2ed84b2b19a5225b15f47e0
Uthaeus <romanlavery@gmail.com>
2023-04-22 19:31:47 -0600

    built quote generator structure

--- src/quote/quote.js
+++ src/quote/quote.js
@@ -1,11 +1,38 @@
+import { useState, useEffect } from "react";
 
 function Quote() {
+  const [quote, setQuote] = useState("");
+  const [author, setAuthor] = useState("");
+  const [quotes, setQuotes] = useState([]);
 
-    return (
-        <div className="quote-container">
-            <h1>Quote</h1>
-        </div>
-    );
+  useEffect(() => {
+    fetch("https://type.fit/api/quotes")
+      .then((response) => response.json())
+      .then((data) => {
+        setQuotes(data);
+        setQuote(data[0].text);
+        setAuthor(data[0].author);
+      })
+      .catch((error) => {
+        console.log("useEffect quote generator", error);
+      });
+  }, []);
+
+  return (
+    <div className="quote-container">
+      <h1>Quote Generator</h1>
+
+      <div className="quote-box">
+        <div className="quote-text"></div>
+        <div className="quote-author"></div>
+      </div>
+
+      <div className="quote-buttons">
+        <button className="quote-button">New Quote</button>
+        <button className="quote-button">Tweet Quote</button>
+      </div>
+    </div>
+  );
 }
 
 export default Quote;
