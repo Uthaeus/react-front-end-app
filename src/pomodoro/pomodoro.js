cd13a61d66f98978bcb56d1b7a99c1387e125aca
Uthaeus <romanlavery@gmail.com>
2023-04-23 21:13:51 -0600

    working on timer functionality

--- src/pomodoro/pomodoro.js
+++ src/pomodoro/pomodoro.js
@@ -4,21 +4,91 @@ import SideControl from './side-control';
 import DisplayControl from './display-control';
 
 function PomodoroClock() {
-    const [display, setDisplay] = useState('25:00');
+    const [display, setDisplay] = useState(1500);
     const [breakTime, setBreakTime] = useState(5);
     const [sessionTime, setSessionTime] = useState(25);
+    const [isRunning, setIsRunning] = useState(false);
+    const [mode, setMode] = useState('session');
+
+    function handleTimeChange(type, change) {
+        if (type === 'break') {
+            if (change === 'inc' && breakTime < 60) {
+                setBreakTime(breakTime + 1);
+            } else if (change === 'dec' && breakTime > 1) {
+                setBreakTime(breakTime - 1);
+            }
+        } else if (type === 'session') {
+            if (change === 'inc' && sessionTime < 60) {
+                setSessionTime(sessionTime + 1);
+            } else if (change === 'dec' && sessionTime > 1) {
+                setSessionTime(sessionTime - 1);
+            }
+        }
+    }
+
+    const timer = () => {
+        if (isRunning) {
+            setTimeout(() => {
+                setDisplay(display - 1);
+                if (display === 0) {
+                    if (mode === 'session') {
+                        setMode('break');
+                        setDisplay(breakTime * 60);
+                    } else if (mode === 'break') {
+                        setMode('session');
+                        setDisplay(sessionTime * 60);
+                    }
+                }
+            }, 1000);
+        }
+    };
+
+    function handlePlay() {
+        setIsRunning(true);
+        timer();
+    }
+
+    function handlePause() {
+        setIsRunning(false);
+        
+    }
+
+    function handleSwitch(m) {
+        if (m === 'break' && mode === 'session') {
+            setMode('break');
+            setDisplay(breakTime * 60);
+        } else if (m === 'session' && mode === 'break') {
+            setMode('session');
+            setDisplay(sessionTime * 60);
+        }
+    }
+
+    function reset() {
+        setDisplay(1500);
+        setBreakTime(5);
+        setSessionTime(25);
+        setIsRunning(false);
+        setMode('session');
+    }
+
+    function format(seconds) {
+        let mins = Math.floor(seconds / 60);
+        let secs = seconds % 60;
+
+        return `${mins < 10 ? '0' : ''}${mins}:${secs < 10 ? '0' : ''}${secs}`;
+    }
 
     return (
         <div className="pomodoro-clock-container">
             <div className="main-display">
-                <p className='main-time'>{display}</p>
+                <p className='main-time'>{format(display)}</p>
             </div>
             <div className="controls">
-                <SideControl type='break' time={breakTime} />
-                <DisplayControl />
-                <SideControl type='session' time={sessionTime} />
+                <SideControl type='break' time={breakTime} handleTimeChange={handleTimeChange} />
+                <DisplayControl reset={reset} handlePlay={handlePlay} handlePause={handlePause} handleSwitch={handleSwitch} />
+                <SideControl type='session' time={sessionTime} handleTimeChange={handleTimeChange} />
             </div>
-            <h1>Pomodoro Clock</h1>
+            
         </div>
     );
 }
--- src/pomodoro/side-control.js
+++ src/pomodoro/side-control.js
@@ -1,13 +1,22 @@
 
 
-function SideControl({type, time}) {
+function SideControl({type, time, handleTimeChange}) {
     let style = type === 'break' ? 'side-control break' : 'side-control session';
+
+    function handleInc() {
+        handleTimeChange(type, 'inc');
+    }
+
+    function handleDec() {
+        handleTimeChange(type, 'dec');
+    }
 
     return (
         <div className={style}>
             <h2 className='side-label'>{type.toUpperCase()}</h2>
             <div className="side-control-buttons">
-
+                <button className="side-control-button" onClick={handleInc}><i class="bi bi-caret-up-fill"></i></button>
+                <button className="side-control-button" onClick={handleDec}><i class="bi bi-caret-down-fill"></i></button>
             </div>
             <p className="side-time">{time}</p>
         </div>
