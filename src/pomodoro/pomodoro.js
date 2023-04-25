4b484def4270e55815670d9f02f9d7ebbe6b7faf
Uthaeus <romanlavery@gmail.com>
2023-04-25 16:53:08 -0600

    built funtionality of timer and controls

--- src/pomodoro/pomodoro.js
+++ src/pomodoro/pomodoro.js
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 
 import SideControl from './side-control';
 import DisplayControl from './display-control';
@@ -9,58 +9,85 @@ function PomodoroClock() {
     const [sessionTime, setSessionTime] = useState(25);
     const [isRunning, setIsRunning] = useState(false);
     const [mode, setMode] = useState('session');
+    let interval;
 
     function handleTimeChange(type, change) {
         if (type === 'break') {
             if (change === 'inc' && breakTime < 60) {
                 setBreakTime(breakTime + 1);
+                if (mode === 'break') {
+                    setDisplay((breakTime + 1) * 60);
+                }
             } else if (change === 'dec' && breakTime > 1) {
                 setBreakTime(breakTime - 1);
+                if (mode === 'break') {
+                    setDisplay((breakTime - 1) * 60);
+                }
             }
         } else if (type === 'session') {
             if (change === 'inc' && sessionTime < 60) {
                 setSessionTime(sessionTime + 1);
+                if (mode === 'session') {
+                    setDisplay((sessionTime + 1) * 60);
+                }
             } else if (change === 'dec' && sessionTime > 1) {
                 setSessionTime(sessionTime - 1);
+                if (mode === 'session') {
+                    setDisplay((sessionTime - 1) * 60);
+                }
             }
         }
     }
 
-    const timer = () => {
-        
-        setInterval(() => {
-            let now = new Date().getTime();
-            let distance = now + 1000;
-            console.log('timer');
-            setDisplay((prev) => {
-                if (now >= distance) {
-                    if (prev - 1 === 0) {
-                        if (mode === 'session') {
+    const timer = (input) => {
+        let seconds = 1000;
+        let now = new Date().getTime();
+        let distance = now + seconds;
+        let currentMode = mode;
+
+        if (!isRunning) {
+            interval = setInterval(() => {
+                now = new Date().getTime();
+                
+                if (now > distance) {
+                    setDisplay((prev) => {
+                        if (prev <= 0 && currentMode === 'session') {
                             setMode('break');
-                            setDisplay(breakTime * 60);
-                        } else if (mode === 'break') {
+                            return breakTime * 60;
+                        } else if (prev <= 0 && currentMode === 'break') {
                             setMode('session');
-                            setDisplay(sessionTime * 60);
+                            return sessionTime * 60;
                         }
-                    } else {
                         return prev - 1;
-                    }
+                    });
+                    distance += seconds;
                 }
-            });
-        }, 1000);
+            }, 400);
+            localStorage.setItem('interval-id', interval);
+        }
+        if (isRunning) {
+            clearInterval(localStorage.getItem('interval-id'));
+        }
+        setIsRunning(input);
     };
 
     function handlePlay() {
-        setIsRunning(true);
-        timer();
+        if (!isRunning) {
+            timer(true);
+        }
     }
 
     function handlePause() {
-        setIsRunning(false);
-        clearInterval(timer);
+        if (isRunning) {
+            timer(false);
+        }
     }
 
     function handleSwitch(m) {
+        if (m !== mode) {
+            clearInterval(localStorage.getItem('interval-id'));
+            setIsRunning(false);
+        }
         if (m === 'break' && mode === 'session') {
             setMode('break');
             setDisplay(breakTime * 60);
@@ -76,6 +103,7 @@ function PomodoroClock() {
         setSessionTime(25);
         setIsRunning(false);
         setMode('session');
+        clearInterval(localStorage.getItem('interval-id'));
     }
 
     function format(seconds) {
