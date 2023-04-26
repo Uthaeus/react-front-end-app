9561889f10a1d14e3c9ba96fc0189a74c4f73ca6
Uthaeus <romanlavery@gmail.com>
2023-04-25 22:42:24 -0600

    creating animation on keystroke for drum machine

--- src/drum/pad.js
+++ src/drum/pad.js
@@ -4,7 +4,12 @@ function Pad({ clip, onClick }) {
 
     useEffect(() => {
         document.addEventListener('keydown', handleKeyPress);
-
+        const pads = document.querySelectorAll('.pad');
+        pads.forEach(pad => {
+            pad.addEventListener('transitionend', () => {
+                pad.classList.remove('playing');
+            });
+        });
         return () => {
             document.removeEventListener('keydown', handleKeyPress);
         }
@@ -16,8 +21,10 @@ function Pad({ clip, onClick }) {
         }
     }
 
-    function playSound() {
+    function playSound(e) {
+        const key = document.getElementById(clip.id);
         const audioTag = document.getElementById(clip.keyTrigger);
+        key.classList.add('playing');
         audioTag.currentTime = 0;
         audioTag.play();
         onClick(clip.keyTrigger);
