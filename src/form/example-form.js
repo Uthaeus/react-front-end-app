cc14b8ad2a802c9d61fd27285295d56757604dd4
Uthaeus <romanlavery@gmail.com>
2023-04-26 14:59:22 -0600

    created initial form and function to switch between components

--- src/form/example-form.js
+++ src/form/example-form.js
@@ -1,14 +1,23 @@
-
+import { useState } from "react";
 
 import Form from "./form";
+import Result from "./result";
 
 function ExampleForm() {
+    const [isForm, setIsForm] = useState(true);
+
+    function submitForm(m) {
+        setIsForm(m);
+    }
 
     return (
         <div className="example-form-container">
             <h1>Example Form</h1>
+            {isForm ? <Form submitForm={submitForm} /> : <Result submitForm={submitForm} />}
         </div>
     );
 }
 
 export default ExampleForm;
+
+
--- src/form/result.js
+++ src/form/result.js
@@ -1,10 +1,12 @@
 
 
-function Result() {
+function Result({ submitForm }) {
 
     return (
         <div>
             <h1>Result</h1>
+
+            <button onClick={() => submitForm(true)}>Back to Form</button>
         </div>
     )
 }
