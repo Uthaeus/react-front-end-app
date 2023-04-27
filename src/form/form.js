1e88905a231b09bce41e37662ed6f0bc6744da1d
Uthaeus <romanlavery@gmail.com>
2023-04-26 20:19:14 -0600

    passing changes to handler, storing in state object

--- src/form/form.js
+++ src/form/form.js
@@ -1,6 +1,26 @@
+import { useState } from 'react';
 
+const initialState = {
+    email: '',
+    name: '',
+    address: '',
+    address2: '',
+    city: '',
+    state: '',
+    zip: '',
+    service: '',
+    usage: '',
+    comment: '',
+    genres: [],
+};
 
 function Form({ submitForm }) {
+    const [formState, setFormState] = useState(initialState);
+
+    function changeHandler(e) {
+        console.log('change', e.target.id, e.target.value);
+        setFormState({ ...formState, [e.target.id]: e.target.value });
+    }
 
     function formSubmitHandler(e) {
         e.preventDefault();
@@ -11,40 +31,42 @@ function Form({ submitForm }) {
         <div className="form-container">
             <form className="row g-3 my-2" onSubmit={formSubmitHandler}>
                 <div className="col-md-6">
-                    <label htmlFor="inputEmail4" className="form-label">Email</label>
-                    <input type="email" className="form-control" id="inputEmail4" />
+                    <label htmlFor="email" className="form-label">Email</label>
+                    <input type="email" className="form-control" id="email" value={formState.email} onChange={changeHandler} />
                 </div>
                 <div className="col-md-6">
-                    <label htmlFor="inputName" className="form-label">Name</label>
-                    <input type="text" className="form-control" id="inputName" />
+                    <label htmlFor="name" className="form-label">Name</label>
+                    <input type="text" className="form-control" id="name" value={formState.name} onChange={changeHandler} />
                 </div>
                 <div className="col-12">
-                    <label htmlFor="inputAddress" className="form-label">Address</label>
-                    <input type="text" className="form-control" id="inputAddress" placeholder="1234 Main St" />
+                    <label htmlFor="address" className="form-label">Address</label>
+                    <input type="text" className="form-control" id="address" placeholder="1234 Main St" value={formState.address} onChange={changeHandler} />
                 </div>
                 <div className="col-12">
-                    <label htmlFor="inputAddress2" className="form-label">Address 2</label>
-                    <input type="text" className="form-control" id="inputAddress2" placeholder="Apartment, studio, or floor" />
+                    <label htmlFor="address2" className="form-label">Address 2</label>
+                    <input type="text" className="form-control" id="address2" placeholder="Apartment, studio, or floor" value={formState.address2} onChange={changeHandler} />
                 </div>
                 <div className="col-md-6">
-                    <label htmlFor="inputCity" className="form-label">City</label>
-                    <input type="text" className="form-control" id="inputCity" />
+                    <label htmlFor="city" className="form-label">City</label>
+                    <input type="text" className="form-control" id="city" value={formState.city} onChange={changeHandler} />
                 </div>
                 <div className="col-md-4">
-                    <label htmlFor="inputState" className="form-label">State</label>
-                    <select id="inputState" className="form-select">
-                        <option selected>Choose...</option>
-                        <option>...</option>
+                    <label htmlFor="state" className="form-label">State</label>
+                    <select id="state" className="form-select" onChange={changeHandler}>
+                        <option value="">Choose...</option>
+                        <option value="UT">UT</option>
+                        <option value="CO">CO</option>
+                        <option value="CA">CA</option>
                     </select>
                 </div>
                 <div className="col-md-2">
-                    <label htmlFor="inputZip" className="form-label">Zip</label>
-                    <input type="text" className="form-control" id="inputZip" />
+                    <label htmlFor="zip" className="form-label">Zip</label>
+                    <input type="text" className="form-control" id="zip" value={formState.zip} onChange={changeHandler} />
                 </div>
                 <div className="col-12">
-                    <label className="form-label" htmlFor='inputService'>Preferred Streaming Service</label>
-                    <select className="form-select" id='inputService'>
-                        <option selected>Choose...</option>
+                    <label className="form-label" htmlFor='service'>Preferred Streaming Service</label>
+                    <select className="form-select" id='service' onChange={changeHandler}>
+                        <option value="">Choose...</option>
                         <option value="Netflix">Netflix</option>
                         <option value="Disney">Disney</option>
                         <option value="Peacock">Peacock</option>
@@ -56,29 +78,95 @@ function Form({ submitForm }) {
                     </select>
                 </div>
                 <div className="col-12">
-                    
+                    <p>How much of your streaming is on this service?</p>
+                    <input type='radio' id='radio-all' value='all' name='usage' />
+                    <label className="me-3" htmlFor="radio-all">: All</label>
+
+                    <input type='radio' id='radio-most' value='most' name='usage' />
+                    <label className="form-label me-3" htmlFor="radio-most">: Most</label>
+
+                    <input type='radio' id='radio-some' value='some' name='usage' />
+                    <label className="form-label me-3" htmlFor="radio-some">: Some</label>
+
+                    <input type='radio' id='radio-none' value='none' name='usage' />
+                    <label className="form-label" htmlFor="radio-none">: None</label>
                 </div>
                 <div className="col-8">
-                    <label className="form-label" htmlFor='inputComment'>Comments:</label>
-                    <textarea id="inputComment" className="form-control" rows={12} />
+                    <label className="form-label" htmlFor='comment'>Comments:</label>
+                    <textarea id="comment" className="form-control" rows={12} value={formState.comment} onChange={changeHandler} />
                 </div>
                 <div className="col-4">
                     <p>Preferred Genres:</p>
                     <div className="row">
                         <div className="col-6">
-                            <div className="form-check">
-                                <input className="form-check-input" type="checkbox" id="checkDrama" />
+                            <div className="form-check mb-2">
+                                <input className="form-check-input" type="checkbox" id="checkDrama" name="genres" value='Drama' />
                                 <label className="form-check-label" htmlFor="checkDrama">
                                     Drama
                                 </label>
+                            </div>
 
+                            <div className="form-check mb-2">
+                                <input className="form-check-input" type="checkbox" id="checkCrime" name="genres" value='Crime' />
+                                <label className="form-check-label" htmlFor="checkCrime">
+                                    Crime
+                                </label>
+                            </div>
+
+                            <div className="form-check mb-2">
+                                <input className="form-check-input" type="checkbox" id="checkWestern" name="genres" value='Western' />
+                                <label className="form-check-label" htmlFor="checkWestern">
+                                    Western
+                                </label>
+                            </div>
+
+                            <div className="form-check mb-2">
+                                <input className="form-check-input" type="checkbox" id="checkRomance" name="genres" value='Romance' />
+                                <label className="form-check-label" htmlFor="checkRomance">
+                                    Romance
+                                </label>
+                            </div>
+
+                            <div className="form-check">
+                                <input className="form-check-input" type="checkbox" id="checkHorror" name="genres" value='Horror' />
+                                <label className="form-check-label" htmlFor="checkHorror">
+                                    Horror
+                                </label>
                             </div>
                         </div>
                         <div className="col-6">
-                            <div className="form-check">
-                                <input className="form-check-input" type="checkbox" id="checkComedy" />
+                            <div className="form-check mb-2">
+                                <input className="form-check-input" type="checkbox" id="checkComedy" name="genres" value='Comedy' />
                                 <label className="form-check-label" htmlFor="checkComedy">
                                     Comedy
+                                </label>
+                            </div>
+
+                            <div className="form-check mb-2">
+                                <input className="form-check-input" type="checkbox" id="checkSciFi" name="genres" value='SciFi' />
+                                <label className="form-check-label" htmlFor="checkSciFi">
+                                    SciFi
+                                </label>
+                            </div>
+
+                            <div className="form-check mb-2">
+                                <input className="form-check-input" type="checkbox" id="checkMystery" name="genres" value='Mystery' />
+                                <label className="form-check-label" htmlFor="checkMystery">
+                                    Mystery
+                                </label>
+                            </div>
+
+                            <div className="form-check mb-2">
+                                <input className="form-check-input" type="checkbox" id="checkAnime" name="genres" value='Anime' />
+                                <label className="form-check-label" htmlFor="checkAnime">
+                                    Anime
+                                </label>
+                            </div>
+
+                            <div className="form-check">
+                                <input className="form-check-input" type="checkbox" id="checkAction" name="genres" value='Action' />
+                                <label className="form-check-label" htmlFor="checkAction">
+                                    Action
                                 </label>
                             </div>
                         </div>
